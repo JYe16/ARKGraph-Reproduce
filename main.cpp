1b94bab7428354d68678c9dce7350a506ba39914
JYe16 <54547626+JYe16@users.noreply.github.com>
2023-11-19 17:51:13 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,34 +7,46 @@
 #include "iostream"
 #include "string"
 #include "fstream"
-#include <math.h>
-#include <unordered_set>
+#include <cmath>
 #include <algorithm>
-#include "limits.h"
 
 using namespace std;
 
-void load_ivecs_data(const string filename, vector<vector<float> > &raw_data, unsigned &num, unsigned &dim);
+struct indexGraph {
+    int bl = 0;
+    int br = 0;
+    int el = 0;
+    int er = 0;
+    vector<int> C;
+};
 
-float calcEulideanDistance(vector<float> v1, vector<float> v2);
+void loadData(const string &filename, vector<vector<float> > &raw_data, unsigned &num, unsigned &dim);
+
+float calcEuclideanDistance(vector<float> v1, vector<float> v2);
 
 bool distanceComparison(const pair<int, float> &a, const pair<int, float> &b);
 
 vector<vector<int>> buildBruteForceIndex(vector<vector<float>> raw_data, int k);
 
+vector<indexGraph> buildCompactGraph(vector<vector<float>> raw_data, int k);
+
+vector<pair<int, float>> calcAndSortDistance(vector<vector<float>> raw_data, vector<float> vi, int index);
+
+vector<int> mergeLR(vector<int> L, vector<int> R);
+
 int main() {
     string data_path = "data/siftsmall_base.fvecs";
     vector<vector<float> > raw_data;
     unsigned dim, num;
-    load_ivecs_data(data_path, raw_data, num, dim);
-//    raw_data.resize(1000);
-    vector<vector<int>> bruteForceIndex = buildBruteForceIndex(raw_data, 10);
-
+    loadData(data_path, raw_data, num, dim);
+    raw_data.resize(100);
+    // vector<vector<int>> bruteForceIndex = buildBruteForceIndex(raw_data, 10);
+    vector<indexGraph> compactIndex = buildCompactGraph(raw_data, 10);
     return 0;
 }
 
 
-void load_ivecs_data(const string filename, vector<vector<float> > &raw_data, unsigned &num, unsigned &dim) {
+void loadData(const string &filename, vector<vector<float> > &raw_data, unsigned &num, unsigned &dim) {
     ifstream in(filename, std::ios::binary);
     if (!in.is_open()) {
         std::cout << "open file error" << std::endl;
@@ -60,48 +72,299 @@ void load_ivecs_data(const string filename, vector<vector<float> > &raw_data, un
 }
 
 
-vector<vector<int>> buildBruteForceIndex(vector<vector<float>> raw_data, int k) {
-    vector<vector<int>> result;
-    vector<vector<pair<int, float>>> distancePairs;
+vector<indexGraph> buildCompactGraph(vector<vector<float>> raw_data, int k) {
 
+    vector<indexGraph> G;
+    int window;
 
     for (int i = 0; i < raw_data.size(); i++) {
-        cout << "Processing " << i << endl;
+        vector<pair<int, float>> sortedDistancePairs = calcAndSortDistance(raw_data, raw_data[i], i);
+        // declare and initialize L and R
+        vector<int> L;
+        vector<int> R;
+
+        int prevLMin = 0;
+        int LMin = 0;
+        int prevRMax = raw_data.size() + 1;
+        int RMax = raw_data.size() + 1;
+
+        for (int j = 0; j < sortedDistancePairs.size(); j++) {
+            int j_val = sortedDistancePairs[j].first;
+            if (j_val < i) {
+                if (j_val > LMin) {
+                    // insert j into L
+                    L.push_back(j_val);
+                    if (L.size() == k + 1) {
+                        prevLMin = L[0];
+                        L.erase(L.begin());
+                        LMin = L[0];
+                    }
+                    sort(L.begin(), L.end());
+                    // merge L and R
+                    if (L.size() + R.size() >= k) {
+                        vector<int> LR = mergeLR(L, R);
+
+                        if (L.size() < k) {
+                            indexGraph graph = *new indexGraph();
+
+                            graph.bl = prevLMin;
+                            graph.br = LR[0];
+                            graph.el = LR[k - 1];
+                            graph.er = LR[k];
+                            for (int z = 0; z < k; z++) {
+                                graph.C.push_back(LR[z]);
+                            }
+                            G.push_back(graph);
+
+                            window = 0;
+
+                            while (window + k < LR.size()) {
+                                indexGraph graph = *new indexGraph();
+
+                                graph.bl = LR[window];
+                                graph.br = LR[window + 1];
+                                graph.el = LR[window + k - 1];
+                                graph.er = LR[window + k];
+                                for (int z = 0; z < k; z++) {
+                                    graph.C.push_back(LR[window + z]);
+                                }
+                                G.push_back(graph);
+                                window++;
+                            }
+
+                            graph = *new indexGraph();;
+
+                            graph.bl = LR[window];
+                            graph.br = LR[window + 1];
+                            graph.el = LR[window + k - 1];
+                            graph.er = prevRMax;
+
+                            for (int z = 0; z < k; z++) {
+                                graph.C.push_back(LR[window + z]);
+                            }
+                            G.push_back(graph);
+
+                        } else if (L.size() == k) {
+                            indexGraph graph = *new indexGraph();;
+
+                            graph.bl = prevLMin;
+                            graph.br = LR[0];
+                            graph.el = i - 1;
+                            graph.er = LR[k];
+
+                            for (int z = 0; z < k; z++) {
+                                graph.C.push_back(LR[window + z]);
+                            }
+                            G.push_back(graph);
+
+                            window = 0;
+
+                            while (window + k < LR.size()) {
+                                graph = *new indexGraph();;
+
+                                graph.bl = LR[window];
+                                graph.br = LR[window + 1];
+                                graph.el = LR[window + k - 1];
+                                graph.er = LR[window + k];
+
+                                for (int z = 0; z < k; z++) {
+                                    graph.C.push_back(LR[window + z]);
+                                }
+                                G.push_back(graph);
+                                window++;
+                            }
+
+                            graph = *new indexGraph();;
+
+                            graph.bl = LR[window];
+                            graph.br = LR[window + 1];
+                            graph.el = LR[window + k - 1];
+                            graph.er = prevRMax;
+
+                            for (int z = 0; z < k; z++) {
+                                graph.C.push_back(LR[window + z]);
+                            }
+                            G.push_back(graph);
+                        }
+                    }
+                }
+
+            } else if (j_val > i) {
+                if (j_val < RMax) {
+                    // insert j into R
+                    R.push_back(j_val);
+                    if (R.size() == k + 1) {
+                        prevRMax = R[R.size() - 1];
+                        R.erase(R.end());
+                        RMax = R[R.size()];
+                    }
+                    sort(R.begin(), R.end());
+
+                    if (L.size() + R.size() >= k) {
+                        vector<int> LR = mergeLR(L, R);
+
+                        if (R.size() < k) {
+                            indexGraph graph = *new indexGraph();
+
+                            graph.bl = prevLMin;
+                            graph.br = LR[0];
+                            graph.el = LR[k];
+                            graph.er = LR[k + 1];
+                            for (int z = 0; z < k; z++) {
+                                graph.C.push_back(LR[z]);
+                            }
+                            G.push_back(graph);
+
+                            window = 0;
+
+                            while (window + k + 1 < LR.size()) {
+                                indexGraph graph = *new indexGraph();
+
+                                graph.bl = LR[window];
+                                graph.br = LR[window + 1];
+                                graph.el = LR[window + k];
+                                graph.er = LR[window + k + 1];
+                                for (int z = 0; z < k; z++) {
+                                    graph.C.push_back(LR[window + z]);
+                                }
+                                G.push_back(graph);
+                                window++;
+                            }
+
+                            graph = *new indexGraph();;
+
+                            graph.bl = LR[window];
+                            graph.br = LR[window + 1];
+                            graph.el = LR[window + k];
+                            graph.er = prevRMax;
+
+                            for (int z = 0; z < k; z++) {
+                                graph.C.push_back(LR[window + z]);
+                            }
+                            G.push_back(graph);
+                        } else if (R.size() == k) {
+//                            if (L.size() == 0)
+                            indexGraph graph = *new indexGraph();
+
+                            graph.bl = prevLMin;
+                            graph.br = LR[0];
+                            graph.el = LR[k - 1];
+                            graph.er = LR[k];
+
+                            for (int z = 0; z < k; z++) {
+                                graph.C.push_back(LR[window + z]);
+                            }
+                            G.push_back(graph);
+
+                            window = 0;
+
+                            while (window + k < LR.size()) {
+                                graph = *new indexGraph();;
+
+                                graph.bl = LR[window];
+                                graph.br = LR[window + 1];
+                                graph.el = LR[window + k - 1];
+                                graph.er = LR[window + k];
+
+                                for (int z = 0; z < k; z++) {
+                                    graph.C.push_back(LR[window + z]);
+                                }
+                                G.push_back(graph);
+                                window++;
+                            }
+
+                            graph = *new indexGraph();;
+
+                            graph.bl = LR[LR.size() - k - 1];
+                            graph.br = i + 1;
+                            graph.el = LR[window + k - 1];
+                            graph.er = prevRMax;
+
+                            for (int z = 0; z < k; z++) {
+                                graph.C.push_back(LR[window + z]);
+                            }
+                            G.push_back(graph);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    cout << G.size();
+    return G;
+}
+
+
+vector<pair<int, float>> calcAndSortDistance(vector<vector<float>> raw_data, vector<float> vi, int index) {
+    vector<pair<int, float>> result;
+    for (int i = 0; i < raw_data.size(); i++) {
+        if (i != index) {
+            pair<int, float> temp_pair;
+            temp_pair.first = i;
+            temp_pair.second = calcEuclideanDistance(raw_data[i], vi);
+            result.push_back(temp_pair);
+        }
+    }
+    sort(result.begin(), result.end(), distanceComparison);
+    return result;
+}
+
+
+vector<vector<int>> buildBruteForceIndex(vector<vector<float>> raw_data, int k) {
+    vector<vector<int>> result;
+
+    for (int i = 0; i < raw_data.size(); i++) {
         vector<pair<int, float>> temp_pairs;
         for (int j = 0; j < raw_data.size(); j++) {
             if (i != j) {
                 pair<int, float> temp_pair;
                 temp_pair.first = j;
-                temp_pair.second = calcEulideanDistance(raw_data[i], raw_data[j]);
+                temp_pair.second = calcEuclideanDistance(raw_data[i], raw_data[j]);
                 temp_pairs.push_back(temp_pair);
             }
         }
+        // sort the key-value pair by distance
         sort(temp_pairs.begin(), temp_pairs.end(), distanceComparison);
-        distancePairs.push_back(temp_pairs);
-    }
-
-    for (int x = 0; x < distancePairs.size(); x++) {
         vector<int> temp_res;
-        for (int y = 0; y < k; y++) {
-            temp_res.push_back(distancePairs[x][y].first);
+        // push k indexes into the result
+        for (int x = 0; x < k; x++) {
+            // if k is greater than the total search key, push k values
+            if (x < temp_pairs.size()) {
+                temp_res.push_back(temp_pairs[x].first);
+            }
         }
         result.push_back(temp_res);
     }
+
     return result;
 }
 
-
+// this function is for sorting the key, value pair
 bool distanceComparison(const pair<int, float> &a, const pair<int, float> &b) {
     return a.second < b.second;
 }
 
 
-float calcEulideanDistance(vector<float> v1, vector<float> v2) {
+float calcEuclideanDistance(vector<float> v1, vector<float> v2) {
     float result = 0.0;
     for (int i = 0; i < v1.size(); i++) {
         result += pow((v1[i] - v2[i]), 2);
     }
     result = sqrt(result);
+    return result;
+}
+
+vector<int> mergeLR(vector<int> L, vector<int> R) {
+    vector<int> result;
+
+    for (int i = 0; i < L.size(); i++) {
+        result.push_back(L[i]);
+    }
+
+    for (int j = 0; j < R.size(); j++) {
+        result.push_back(R[j]);
+    }
+
     return result;
 }
 
